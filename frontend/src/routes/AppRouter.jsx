aeb115fa5f7c0e833ada048dc2a52fc66e9d7147
unknown <ezequielafeita@gmail.com>
2024-08-22 12:18:20 -0300

    Se agregó la base proveída por el profesor

---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "../views/login/Login";
+import { Register } from "../views/register/Register";
+import { Tareas } from "../views/tareas/Tareas";
+import PrivateRoutes from "./PrivateRoutes";
+import PublicRoutes from "./PublicRoutes";
+import { NuevaTarea } from "../views/tareas/nueva-tarea/NuevaTarea";
+import { EditarTarea } from "../views/tareas/editar-tarea/EditarTarea";
+import { EliminarTarea } from "../views/tareas/eliminar-tarea/EliminarTarea";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PublicRoutes />}>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route element={<PrivateRoutes />}>
+          <Route path="/tareas" element={<Tareas />} />
+          <Route path="/tareas/nueva-tarea" element={<NuevaTarea />} />
+          <Route path="/tareas/editar-tarea/:id" element={<EditarTarea />} />
+          <Route
+            path="/tareas/eliminar-tarea/:id"
+            element={<EliminarTarea />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
