b7627c6fa702050a29a492bbfaf2d3a5f0e090f6
unknown <ezequielafeita@gmail.com>
2024-08-23 12:03:12 -0300

    Se agregaron identificadores del login

--- client/assets/js/login.js
+++ client/assets/js/login.js
@@ -1,6 +1,21 @@
 const container = document.getElementById("container");
 const registerBtn = document.getElementById("register");
 const loginBtn = document.getElementById("login");
+const register = document.getElementById("registrar");
+
+register.addEventListener("click", () => {
+  const nombre = document.querySelector("#iN").value;
+  const apellido = document.querySelector("#iA").value;
+  const dni = document.querySelector("#iD").value;
+  const hashContrasenia = "lol";
+  const info = {
+    nombre,
+    apellido,
+    dni,
+    hashContrasenia,
+  };
+  console.log(info);
+});
 
 registerBtn.addEventListener("click", () => {
   container.classList.add("active");
