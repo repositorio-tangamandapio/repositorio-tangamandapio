8b7a48c62685fabbc69350b178f4225bbcab9526
Gaston Espinola <gastonespinola146@gmail.com>
2024-08-23 12:06:28 -0300

    se agrego el JavaScript de imagenes

---
+++ client/assets/js/imagenes.js
@@ -0,0 +1,74 @@
+const img = [
+    {
+        "nombre":"MANDIOCA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/472487/Mandioca-por-Kg-1-11041.jpg?v=636694698402470000"
+    },
+    {
+        "nombre":"TOMATE",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/339429-800-auto?v=636393043636870000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"LECHUGA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/472809-800-auto?v=636695562314000000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"ZAPALLO",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/472994-800-auto?v=636695569887800000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"PAPA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/449110-800-auto?v=636567690053330000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"CEBOLLA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/698610-800-auto?v=637871997724330000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"PEPINO",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/242854-800-auto?v=636386303451070000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"MORRON",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/339349-800-auto?v=636393041930400000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"PEREJIL",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/424308-800-auto?v=636486511466970000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"CEBOLLITA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/183189-800-auto?v=636383441363470000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"CILANTRO",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/697199-800-auto?v=637858930903370000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"CHAUCHA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/182131-800-auto?v=636383425328830000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"ZANAHORIA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/472800-800-auto?v=636695562251270000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"LIMON",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/181777-800-auto?v=636383419335130000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"CHOCLO",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/181712-800-auto?v=636383417812800000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"PERA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/180103-800-auto?v=636383390562770000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"UVA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/472992-800-auto?v=636695568808430000&width=800&height=auto&aspect=true"
+    },
+    {
+        "nombre":"MANZANA",
+        "img": "https://jumboargentina.vtexassets.com/arquivos/ids/472481-800-auto?v=636694698370130000&width=800&height=auto&aspect=true"
+    },
+]
